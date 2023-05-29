af73e55048d5750a030a7ab496f393286658b934
Femi <femi.olakanmi@yahoo.com>
2023-05-29 23:43:54 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function myNews() {
+    document.getElementById("newsletter").style.display = "block";
+}
+function myClose() {
+    document.getElementById("newsletter").style.display = "none";
+}
